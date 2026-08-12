5b8ff5182e030d05f29a15069cdb4bd9cdc634db
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2026-08-12 19:51:56 +0530

    remove from cart functionality achieved

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,16 @@ function App() {
   const reducer = (state, action) => {
     if (action.type == "ADD_TO_CART") {
       // check if the product is already in the cart
-      const existsProduct = state.cart.find(
+      const existingProduct = state.cart.find(
         (item) => item.id === action.payload.id,
       );
 
       // if yes then increase quantiy
-      if (existsProduct) {
+      if (existingProduct) {
         return {
           ...state,
           cart: state.cart.map((item) => {
-            if (existsProduct.id === item.id) {
+            if (existingProduct.id === item.id) {
               return {
                 ...item,
                 quantity: item.quantity + 1,
@@ -43,6 +43,30 @@ function App() {
               quantity: 1,
             },
           ],
+        };
+      }
+    }
+
+    // REMOVE FROM CART
+    if (action.type == "REMOVE_FROM_CART") {
+      // we need to check the quantity of the product if its 1 then we will remove it all together if its not one then we will only decrese the quantity
+      if (action.payload.quantity > 1) {
+        return {
+          ...state,
+          cart: state.cart.map((item) => {
+            if (item.id === action.payload.id) {
+              return {
+                ...item,
+                quantity: item.quantity - 1,
+              };
+            }
+            return item;
+          }),
+        };
+      } else {
+        return {
+          ...state,
+          cart: state.cart.filter((item) => item.id !== action.payload.id),
         };
       }
     }
@@ -78,8 +102,26 @@ function App() {
           <h3>{item.name}</h3>
           <p>{item.price}</p>
           <p>Quantity: {item.quantity}</p>
-          <button>+</button>
-          <button>-</button>
+          <button
+            onClick={() =>
+              dispatch({
+                type: "ADD_TO_CART",
+                payload: item,
+              })
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={() =>
+              dispatch({
+                type: "REMOVE_FROM_CART",
+                payload: item,
+              })
+            }
+          >
+            -
+          </button>
         </div>
       ))}
     </div>
