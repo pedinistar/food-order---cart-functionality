0205a12f755170ddacffb8d724f1c97dbe14f2a7
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2026-08-13 21:54:22 +0530

    implement styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import "./App.css";
 
 function App() {
   const products = [
@@ -12,7 +13,7 @@ function App() {
   };
 
   const reducer = (state, action) => {
-    if (action.type == "ADD_TO_CART") {
+    if (action.type === "ADD_TO_CART") {
       // check if the product is already in the cart
       const existingProduct = state.cart.find(
         (item) => item.id === action.payload.id,
@@ -48,7 +49,7 @@ function App() {
     }
 
     // REMOVE FROM CART
-    if (action.type == "REMOVE_FROM_CART") {
+    if (action.type === "REMOVE_FROM_CART") {
       // we need to check the quantity of the product if its 1 then we will remove it all together if its not one then we will only decrese the quantity
       if (action.payload.quantity > 1) {
         return {
@@ -78,6 +79,8 @@ function App() {
         cart: [],
       };
     }
+
+    return state;
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -87,70 +90,80 @@ function App() {
   }, 0);
 
   return (
-    <div>
+    <div className="main">
       {/* Products */}
-      {products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <p>{product.price}</p>
-          <button
-            onClick={() =>
-              dispatch({
-                type: "ADD_TO_CART",
-                payload: product,
-              })
-            }
-          >
-            Add
-          </button>
-        </div>
-      ))}
+      <div>
+        <h3>Products</h3>
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <h3>{product.name}</h3>
+            <p>&#8377; {product.price}</p>
+            <button
+              onClick={() =>
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: product,
+                })
+              }
+            >
+              Add
+            </button>
+          </div>
+        ))}
+      </div>
 
       {/* Cart */}
-      <hr />
-      <h3>Cart</h3>
-      {state.cart.map((item) => (
-        <div key={item.id}>
-          <h3>{item.name}</h3>
-          <p>{item.price}</p>
-          <p>Quantity: {item.quantity}</p>
-          <p>Total: {item.price * item.quantity}</p>
+      <div>
+        <h3>Cart</h3>
+        {state.cart.map((item) => (
+          <div className="cart-item" key={item.id}>
+            <h3>{item.name}</h3>
+
+            <div className="quantity">
+              <button
+                onClick={() =>
+                  dispatch({
+                    type: "ADD_TO_CART",
+                    payload: item,
+                  })
+                }
+              >
+                +
+              </button>
+              <p>{item.quantity}</p>
+              <button
+                onClick={() =>
+                  dispatch({
+                    type: "REMOVE_FROM_CART",
+                    payload: item,
+                  })
+                }
+              >
+                -
+              </button>
+            </div>
+
+            <p className="price">&#8377; {item.price * item.quantity}</p>
+
+            <hr />
+          </div>
+        ))}
+
+        {totalBill > 0 && <h4 className="total">Total: &#8377; {totalBill}</h4>}
+
+        {state.cart.length !== 0 && (
           <button
+            className="clear-btn"
             onClick={() =>
               dispatch({
-                type: "ADD_TO_CART",
-                payload: item,
+                type: "CLEAR_CART",
               })
             }
           >
-            +
+            Clear Cart
           </button>
-          <button
-            onClick={() =>
-              dispatch({
-                type: "REMOVE_FROM_CART",
-                payload: item,
-              })
-            }
-          >
-            -
-          </button>
-        </div>
-      ))}
-
-      {totalBill === 0 ? null : <h4>Total: ${totalBill}</h4>}
-
-      {state.cart.length !== 0 && (
-        <button
-          onClick={() =>
-            dispatch({
-              type: "CLEAR_CART",
-            })
-          }
-        >
-          Clear Cart
-        </button>
-      )}
+        )}
+      </div>
     </div>
   );
 }
